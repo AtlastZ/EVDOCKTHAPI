5ec6f86c55716fac96214c06e5fa9db7af09970d
AnalyzyA <analyzya@gmail.com>
2023-12-15 18:04:49 +0700

    main

--- main.go
+++ main.go
@@ -2,24 +2,69 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
 	"time"
 )
+func fetchDataFromDBJSON(w http.ResponseWriter, db *sql.DB) {
+	// Execute the SQL query
+	log.Println("Fetching JSON data from database")
+	rows, err := db.Query("SELECT * FROM data1")
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal Server Error JSON", http.StatusInternalServerError)
+		return
+	}
+	log.Println("Fetch JSON data from database successfully")
+	defer rows.Close()
+
+	// Create a slice to hold the retrieved data
+	var data []map[string]interface{}
+
+	// Process the query results
+	var id int
+	var skill string
+
+	for rows.Next() {
+		err := rows.Scan(&id, &skill)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "Internal Server Error API", http.StatusInternalServerError)
+			return
+		}
+
+		// Create a map for each row
+		rowData := map[string]interface{}{
+			"ID":   id,
+			"skill": skill,
+		}
+
+		// Append the map to the data slice
+		data = append(data, rowData)
+	}
+
+	// Encode the data slice as JSON and write it to the response writer
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(data)
+}
+
+// apiHandlerJSON is an HTTP handler function for the API that returns data as JSON.
+func apiHandlerJSON(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	fetchDataFromDBJSON(w, db)
+}
 
 // fetchDataFromDB retrieves data from the database and writes it to the response writer.
 func fetchDataFromDB(w http.ResponseWriter, db *sql.DB) {
 	// Execute the SQL query
-	log.Println("Fetching data from database..")
 	rows, err := db.Query("SELECT * FROM data1")
 	if err != nil {
 		log.Println(err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-	log.Println("Done fetching data from database")
 	defer rows.Close()
 
 	// Process the query results
@@ -36,16 +81,17 @@ func fetchDataFromDB(w http.ResponseWriter, db *sql.DB) {
 		// You can process the retrieved data here
 		fmt.Fprintf(w, "ID: %d, Name: %s\n", id, skill)
 	}
-	log.Println("Successfully retrieved")
 }
 
 // dbConnect connects to the database and returns a *sql.DB instance.
 func dbConnect() (*sql.DB, error) {
 	// Open a database connection
+	log.Println("Connecting to database..")
 	db, err := sql.Open(DBDriver, fmt.Sprintf("%s:%s@%s", DBUser, DBPassword, DBName))
 	if err != nil {
 		return nil, err
 	}
+	log.Println("Connected to database.")
 	return db, nil
 }
 
