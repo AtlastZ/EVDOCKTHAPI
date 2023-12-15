062c0433084cd124f985d19f81addca3ffd2e7ea
AnalyzyA <analyzya@gmail.com>
2023-12-15 17:21:34 +0700

    main

---
+++ AllAsJson.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	// "fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"net/http"
+	// "time"
+)
+// fetchDataFromDBJSON retrieves data from the database and writes it as JSON to the response writer.
+func fetchDataFromDBJSON(w http.ResponseWriter, db *sql.DB) {
+	// Execute the SQL query
+	rows, err := db.Query("SELECT * FROM data1")
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	// Create a slice to hold the retrieved data
+	var data []map[string]interface{}
+
+	// Process the query results
+	var id int
+	var name string
+
+	for rows.Next() {
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		// Create a map for each row
+		rowData := map[string]interface{}{
+			"ID":   id,
+			"Name": name,
+		}
+
+		// Append the map to the data slice
+		data = append(data, rowData)
+	}
+
+	// Encode the data slice as JSON and write it to the response writer
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(data)
+}
+
+// apiHandlerJSON is an HTTP handler function for the API that returns data as JSON.
+func apiHandlerJSON(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	fetchDataFromDBJSON(w, db)
+}
--- hello.go
+++ hello.go
@@ -1,17 +1,62 @@
 
 package main
 
-// import (
-// 	"fmt"
-// 	"net/http"
-// 	"time"
-// )
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"net/http"
+	"time"
+)
 
-// func greet(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintf(w, "Hello World! %s", time.Now())
-// }
+// fetchDataFromDB retrieves data from the database and writes it to the response writer.
+func fetchDataFromDB(w http.ResponseWriter, db *sql.DB) {
+	// Execute the SQL query
+	rows, err := db.Query("SELECT * FROM data1")
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
 
-// func main() {
-// 	http.HandleFunc("/", greet)
-// 	http.ListenAndServe(":8080", nil)
-// }
+	// Process the query results
+	var id int
+	var skill string
+
+	for rows.Next() {
+		err := rows.Scan(&id, &skill)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		// You can process the retrieved data here
+		fmt.Fprintf(w, "ID: %d, Name: %s\n", id, skill)
+	}
+}
+
+// dbConnect connects to the database and returns a *sql.DB instance.
+func dbConnect() (*sql.DB, error) {
+	// Open a database connection
+	db, err := sql.Open(DBDriver, fmt.Sprintf("%s:%s@%s", DBUser, DBPassword, DBName))
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+// apiHandler is an HTTP handler function for the API.
+func apiHandler(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	fetchDataFromDB(w, db)
+}
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	location, err := time.LoadLocation("Asia/Bangkok")
+	if err != nil {
+		fmt.Println("Error loading location:", err)
+		return
+	}
+	fmt.Fprintf(w, "Hello World! %s", (time.Now().In(location)))
+}
