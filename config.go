7de4f16aee1d39d4ccf2c7962526e0c3e5163717
AnalyzyA <analyzya@gmail.com>
2023-12-15 17:52:48 +0700

    main

--- config.go
+++ config.go
@@ -3,7 +3,7 @@ package main
 
 const (
 	DBDriver   = "mysql"
-	DBUser     = "root"
-	DBPassword = "@Password0"
+	DBUser     = "api"
+	DBPassword = "@PAssword0"
 	DBName     = "tcp(3.25.177.42:3306)/96hp"
 )
